76b8e85984f2705dc003521393788b1dabdbf969
Jessica Trac <jessicamtrac@gmail.com>
2018-11-16 17:27:38 -0500

    adding no-plaque control image and testing its histogram

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import sharpening
 import Color_Histogram
 
 def main():
-              
+        
+    # Testing image with plaque      
 	raw_image = cv2.imread('plaque.jpg',1)
 	sharpened_image = sharpening.sharpen(raw_image)
 	thresholding_image = localization.localization(sharpened_image)
 	Color_Histogram.fluorescence_hist(sharpened_image)
+
+	# # Testing image with no plaque
+	# raw_image = cv2.imread('no_plaque.jpg',1) 
+	# sharpened_image = sharpening.sharpen(raw_image)
+	# thresholding_image = localization.localization(sharpened_image)
+	# Color_Histogram.fluorescence_hist(sharpened_image)
 
 main()
