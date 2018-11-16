dbf28dbd1c1836fba77207d599a8b2130390b914
unknown <dayals@mcmaster.ca>
2018-11-16 16:36:06 -0500

    added histrogram function to main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import localization
 import cv2
 import sharpening
+import Color_Histogram
 
 def main():
               
 	raw_image = cv2.imread('plaque.jpg',0)
 	sharpened_image = sharpening.sharpen(raw_image)
-	localization.localization(sharpened_image)
+	thresholding_image = localization.localization(sharpened_image)
+	Color_Histogram.fluorescence_hist(thresholding_image)
 
 main()
